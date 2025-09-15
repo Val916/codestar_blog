e2e7be98c94034f8f866888baf0d5e88816aaad8
Val916 <junsofdev@proton.me>
2025-09-15 15:35:23 +0100

    add a Post model

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_k76wFZJAuIpo@ep-rapid-breeze-ag1za5wc.c-2.eu-central-1.aws.neon.tech/gravy_iron_salt_885364"
 )
+os.environ.setdefault("SECRET_KEY", "1ElgpnkWMTMKLppdYl4M1I0Ltn73IIICXlXxjYk3w6mfU")
