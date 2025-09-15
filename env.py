897e32fd6e6d04f2ed8577ee4682390148706311
Val916 <junsofdev@proton.me>
2025-09-15 13:08:22 +0100

    deployment settings

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_k76wFZJAuIpo@ep-rapid-breeze-ag1za5wc.c-2.eu-central-1.aws.neon.tech/gravy_iron_salt_885364"
+)
